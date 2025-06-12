2279e92eada98cf2b5867f5e9198279fb778cc5d
Yash <bajajy82@gmail.com>
2025-06-13 00:00:58 +0530

    feat: add StreamableHTTPServerTransport Mcp api routes

---
+++ src/routes/api.mcp.$id.tsx
@@ -0,0 +1,106 @@
+import { json } from "@tanstack/react-start";
+import { createAPIFileRoute } from "@tanstack/react-start/api";
+
+import { randomUUID } from "node:crypto";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
+import { isInitializeRequest } from "@modelcontextprotocol/sdk/types.js";
+import { getEvent } from "vinxi/http";
+import { z } from "zod";
+const transports: { [sessionId: string]: StreamableHTTPServerTransport } = {};
+
+type VinxiRequest = ReturnType<typeof getEvent>["node"]["req"];
+type VinxiResponse = ReturnType<typeof getEvent>["node"]["res"];
+
+const handleSessionRequest = async (req: VinxiRequest, res: VinxiResponse) => {
+	const sessionId = req.headers["mcp-session-id"] as string | undefined;
+	if (!sessionId || !transports[sessionId]) {
+		res.statusCode = 400;
+		res.end("Invalid or missing session ID");
+		return;
+	}
+
+	const transport = transports[sessionId];
+	await transport.handleRequest(req, res);
+};
+
+export const APIRoute = createAPIFileRoute("/api/mcp/$id")({
+	// @ts-expect-error
+	GET: async ({ request, params }) => {
+		const req = getEvent().node.req;
+		const res = getEvent().node.res;
+
+		handleSessionRequest(req, res);
+	},
+	// @ts-expect-error
+	DELETE: async ({ request }) => {
+		const req = getEvent().node.req;
+		const res = getEvent().node.res;
+
+		handleSessionRequest(req, res);
+	},
+
+	// @ts-expect-error
+	POST: async ({ request, params }) => {
+		// const sessionId = req.headers['mcp-session-id'] as string | undefined;
+		const sessionId = request.headers.get("mcp-session-id") || undefined;
+		let transport: StreamableHTTPServerTransport;
+		const body = await request.json();
+		if (sessionId && transports[sessionId]) {
+			// Reuse existing transport
+			transport = transports[sessionId];
+		} else if (!sessionId && isInitializeRequest(body)) {
+			// New initialization request
+			transport = new StreamableHTTPServerTransport({
+				sessionIdGenerator: () => randomUUID(),
+				onsessioninitialized: (sessionId) => {
+					// Store the transport by session ID
+					transports[sessionId] = transport;
+				},
+			});
+
+			// Clean up transport when closed
+			transport.onclose = () => {
+				if (transport.sessionId) {
+					delete transports[transport.sessionId];
+				}
+			};
+			const server = new McpServer({
+				name: "example-server",
+				version: "1.0.0",
+			});
+
+			// ... set up server resources, tools, and prompts ...
+
+			server.tool(
+				"roll_dice",
+				"Rolls an N-sided die",
+				{ sides: z.number().int().min(2) },
+				async ({ sides }) => {
+					const value = 1 + Math.floor(Math.random() * sides);
+					return {
+						content: [{ type: "text", text: `🎲 You rolled a ${value}!` }],
+					};
+				},
+			);
+
+			// Connect to the MCP server
+			await server.connect(transport);
+		} else {
+			return json(
+				{
+					error: "Bad Request: No valid session ID provided",
+					code: -32000,
+				},
+				{ status: 400 },
+			);
+		}
+
+		// Handle the request
+		await transport.handleRequest(
+			getEvent().node.req,
+			getEvent().node.res,
+			body,
+		);
+	},
+});
